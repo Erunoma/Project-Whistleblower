664026bb37b603566ea57a215e03738273143ccc
Erunoma <oliver.boots@hotmail.com>
2023-03-10 12:03:13 +0100

    web-branch-0.02
    - Added debug and local mode to website hosting
    - Added a chart for website (WIP)

--- Website/Code/app.py
+++ Website/Code/app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 
 app = Flask("testapp")
 
+#Mode modules
+localMode=False
+debugMode=False
+
+#Variables
 publicVar=0
 
 @app.route("/")
-
 def main():
     var=0
     var+=1
@@ -13,14 +17,18 @@ def main():
     #in the render_template section
     return render_template("main.html", publicVar=var)
 
-
 #https://stackoverflow.com/questions/31965558/how-to-display-a-variable-in-html
 #https://flask.palletsprojects.com/en/2.2.x/tutorial/templates/
 
+#Hosting code. If localMode is False, the site will be hosted on the network. 
+#Make sure to only disable localMode on safe networks.
+#If debug mode is enabled, the site will be hosted locally in debug mode.
+if localMode==False:
+    if __name__=="__main__":
+        app.run(host="0.0.0.0", port=80)
+elif debugMode==True:
+    app.run(host="127.0.0.1", port=5000, debug=True)
+else:   
+    app.run(host="127.0.0.1", port=5000)    
 
-
-
-
-
-#To make the website viewable online, use this code in the terminal:
-#$ flask run --host=0.0.0.0
+#https://stackoverflow.com/questions/17309889/how-to-debug-a-flask-app
