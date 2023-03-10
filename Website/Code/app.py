f004e43715e4199db7fb1c239ad3210102bf7d3c
Erunoma <oliver.boots@hotmail.com>
2023-03-10 10:22:36 +0100

    web-branch-0.01
    - Added template functionality (see the documentation)
    - Added variable example
    - Created a Charts folder in others

--- Website/Code/app.py
+++ Website/Code/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
+app = Flask("testapp")
+
+publicVar=0
 
 @app.route("/")
-def home():
-    return "This is a test"
+
+def main():
+    var=0
+    var+=1
+    #To output a variable to the server, the variable needs to be sent to the HTML document
+    #in the render_template section
+    return render_template("main.html", publicVar=var)
+
+
+#https://stackoverflow.com/questions/31965558/how-to-display-a-variable-in-html
+#https://flask.palletsprojects.com/en/2.2.x/tutorial/templates/
+
+
+
+
+
+
+#To make the website viewable online, use this code in the terminal:
+#$ flask run --host=0.0.0.0
