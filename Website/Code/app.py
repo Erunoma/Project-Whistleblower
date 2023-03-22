f999cd440c8ac27cc0ba1008422e5b584a0cbb54
Erunoma <oliver.boots@hotmail.com>
2023-03-22 11:56:13 +0100

    web branch 0.22
    - The server now creates a database
    - removed unused files
    - added more layout images

--- Website/Code/app.py
+++ Website/Code/app.py
@@ -1,8 +1,28 @@
+
 from flask import Flask, render_template, request
-import connect
 import sys
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask("testapp")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whistlebase.db'
+#initilize database
+db = SQLAlchemy(app)
+
+class Whistlebase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lotCor = db.Column(db.Float)
+    latCor = db.Column(db.Float)
+    macAdd = db.Column(db.String(200))
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __init__(self, id, lotCor, latCor ,macAdd, date_added):
+        self.id = id
+        self.lotCor = lotCor
+        self.latCor = latCor
+        self.macAdd = macAdd
+        self.date_added = date_added
 
 #Mode modules
 localMode=True
@@ -18,25 +38,26 @@ debugMode=False
 #with the button. 
 @app.route("/", methods=["GET", "POST"])
 def main():
-    
+
     if request.method== "POST":
         if request.form["action1"] == "GPS":
             lon=55.687101712605624
             lan=12.613155975026007
             gogurl="https://www.google.com/maps/search/?api=1&query="+str(lon)+","+str(lan)
             return render_template("gps.html",links=gogurl)
-        
+        if request.form["action1"] == "Database":
+            return render_template("database.html")
+        if request.form["action1"] == "Profiles":
+            return render_template("profiles.html")
+         
     elif request.method == "GET":
         return render_template("main.html")
 
-#This is code for the index website. app route creates the url.
-@app.route("/login", methods =["POST", "GET"])
-def login():
-    return render_template("index.html")
 
-@app.route("/<name>", methods =["POST", "GET"])
-def index(name):
-    return render_template("index.html", username = name)
+
+with app.app_context():
+      print("Database created")
+      db.create_all()
 
 
 #https://stackoverflow.com/questions/31965558/how-to-display-a-variable-in-html
@@ -49,9 +70,12 @@ if localMode==False:
     if __name__=="__main__":
         app.run(host="0.0.0.0", port=80)
 elif debugMode==True:
-    app.run(host="127.0.0.1", port=5000, debug=True)
-else:   
-    app.run(host="127.0.0.1", port=5000)    
+    if __name__=="__main__":
+        app.run(host="127.0.0.1", port=5000, debug=True)
+else:
+    if __name__=="__main__":   
+      
+        app.run(host="127.0.0.1", port=5000)    
 
 
 #https://stackoverflow.com/questions/17309889/how-to-debug-a-flask-app
