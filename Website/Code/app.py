4d328e9d14c6624e6bd88634baac6bb37b909919
sign281g <sign281g@stud.kea.dk>
2023-03-27 12:32:14 +0200

    Update app.py

--- Website/Code/app.py
+++ Website/Code/app.py
@@ -1,16 +1,16 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request # Imports flask-module 
 import sys
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import sqlite3 
 from flask import g
-import socket
+import socket 
 from threading import Thread
 import threading
 
 
-
+# Creates flask-app names "testapp"
 app = Flask("testapp")
 
 #Mode modules
@@ -21,7 +21,7 @@ createNewTable=False
 
 isDatabaseBusy = False
 
-
+# Assign variables, create the global variable "sentdata" and assign it to an empty list  
 imported_id = 0
 imported_lotCor = 0.0
 imported_latCor= 0.0
@@ -30,14 +30,17 @@ imported_dateTime= "2/3"
 global sentdata
 sentdata=[]
 
+# the name "whistlebase" gets assigned to an sqlite3-connection to a database called "whistlebase.db". Therefore, refering to the connection is now "whistlebase".  
 whistlebase = sqlite3.connect('whistlebase.db', check_same_thread=False)
 
+# A function used for creating a table in the database. Takes 5 args listed in the (). Makes a commit with the listed fields to the database and afterwards, prints "A new table has been created". 
 def create_table():
     cur = whistlebase.cursor()
     cur.execute("CREATE TABLE log (id int primarykey, lotCor float, latCor float, macAdd string, datetime string)")
     whistlebase.commit()
     print("A new table has been created")
 
+# A function used for inserting the 5 given args listed above into the database with the collected data. After doing the commit, prints "new log has been added" 
 def add_db_data():
     global imported_id
     global imported_lotCor
@@ -51,19 +54,21 @@ def add_db_data():
     whistlebase.commit()
     print("new log has been added")
 
-
+# A function used to check for connection to the db. 
 def get_db_connection():
     conn = sqlite3.connect('whistlebase.db')
     conn.row_factory = sqlite3.Row
     print("Connection established to db")
     return conn
-    
+
+# Function used to clear the log in the database. Executes the clear by doing a commit. Afterwards, prints "database has been cleared".    
 def clear_db():
     cur = whistlebase.cursor()
     cur.execute("DELETE FROM log")
     whistlebase.commit
     print("database has been cleared")
 
+# If-statements checking the variables in the top of the script. If the condition is True, a function is called. Goes for both if-statements seen below. 
 if createNewTable == True:
     create_table()
 
@@ -73,7 +78,9 @@ if dbTestMode == True:
     #add_db_data()
     get_db_connection()
 
-
+# a function creating the socket for the website. Binds on port 3000 on local network. If connection is established and listening, a condition evaluates to True.
+# If so, data is sent to and recieved by the database assigning it to the variable "newSentData" The sent data is decoded to utf-8-format and splitted.  
+# At the end of the function, the connection is closed. 
 def init_socket():
     global s
     global newSentData
@@ -96,7 +103,8 @@ def init_socket():
         clientsocket.send(stringToSend.encode())
         s.close()
         init_thread()
-        
+      
+# This function is used above when the data is converted. A print-function prints the data in convert. 
 def convert_and_apply():
     global imported_id
     global imported_lotCor
@@ -107,6 +115,10 @@ def convert_and_apply():
     print("Converting...")
     print("This is the new sent data in convert : ",newSentData)
 
+# Variable-names are assigned to values from the list with the name "newSentData". These variables also get converted into strings, floats and integers.
+# A print with the data recieved is executed.
+# The function add_db_data described above is called.
+# An exeption is throwed if an error occurs. A print with the message "An error has occured" is shown. 
     try:
         imported_id = int(newSentData[0])
         imported_lotCor = float(newSentData[1])
@@ -118,17 +130,13 @@ def convert_and_apply():
         add_db_data()
     except Exception as e: print("An error has occured: ",e)
      
-       
-    
-    
-    
+
+# A function initiating a thread. The target is the init_socket from above. The thread is started and joined. A print with the message "New thread started" is shown.      
 def init_thread():
     th = Thread(target=init_socket)
     th.start()
     th.join()
-    print("New thread started")
-        
-       
+    print("New thread started") 
         
    
 #https://docs.python.org/3/library/socket.html#creating-sockets
